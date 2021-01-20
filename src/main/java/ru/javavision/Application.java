cdafeb59aa47a9308f7762879340dd2630e1a7cb
Navatar <umdomby@yandex.ru>
2021-01-20 10:02:56 +0300

    update

--- src/main/java/ru/javavision/Application.java
+++ src/main/java/ru/javavision/Application.java
@@ -5,33 +5,33 @@ package ru.javavision;
  * Created : 02/11/2017.
  */
 public class Application {
-//    public static void main(String[] args) {
-//
-//        PhoneDAO phoneDAO = null;
-//
-//        try {
-////            phoneDAO = new PhoneDAOImpl("postgres", "1", "jdbc:postgresql://localhost:5432/phones_magazine");
-////
-////            StatisticRepository dao =  new StatisticRepositoryImpl("postgres", "1", "jdbc:postgresql://localhost:5432/phones_magazine");
-////
-////            final PhoneModel model = new PhoneModel();
-////            model.setName("samsung");
-////            List<PhoneModel> models = new ArrayList<>();
-////            models.add(model);
-////            dao.getStat(
-////                    models,
-////                    new Statistic.TimeRange(
-////                            new Timestamp(System.currentTimeMillis() - 31536000000L),
-////                            new Timestamp(System.currentTimeMillis())),
-////                    Comparator.comparing(Statistic::getRevenue)
-////                    ).forEach(System.out::println);
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } finally {
-//            assert phoneDAO != null;
-//            phoneDAO.closeConnection();
-//        }
+    public static void main(String[] args) {
+
+        PhoneDAO phoneDAO = null;
+
+        try {
+            phoneDAO = new PhoneDAOImpl("postgres", "1", "jdbc:postgresql://localhost:5432/phones-magazine");
+
+            StatisticRepository dao =  new StatisticRepositoryImpl("postgres", "1", "jdbc:postgresql://localhost:5432/phones_magazine");
+
+            final PhoneModel model = new PhoneModel();
+            model.setName("samsung");
+            List<PhoneModel> models = new ArrayList<>();
+            models.add(model);
+            dao.getStat(
+                    models,
+                    new Statistic.TimeRange(
+                            new Timestamp(System.currentTimeMillis() - 31536000000L),
+                            new Timestamp(System.currentTimeMillis())),
+                    Comparator.comparing(Statistic::getRevenue)
+                    ).forEach(System.out::println);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            assert phoneDAO != null;
+            phoneDAO.closeConnection();
+        }
 //    }
 
 }
